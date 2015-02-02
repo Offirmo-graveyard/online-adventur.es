f459ebedc2d91795e79bcae44eddf6a2308b0c23
Offirmo <offirmo.net@gmail.com>
2015-02-02 01:07:15 +0100

    nice server-side page layout + enriched headers (social network...)

--- server/web/express-app.js
+++ server/web/express-app.js
@@ -12,6 +12,7 @@ module.exports = app;
 var consolidated_templates = require('consolidate'); // always needed
 // now require all templating engines we wish to use
 var dust = require('dustjs-linkedin'); // http://dejanglozic.com/2014/01/27/dust-js-such-templating/
+require('dustjs-helpers'); // also
 dust.optimizers.format = function(ctx, node) { return node; }; // https://github.com/linkedin/dustjs/wiki/Dust-Tutorial#controlling-whitespace-suppression
 app.engine('dust', consolidated_templates.dust); // .dust will be rendered with...
 
