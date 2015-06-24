827b8ab5a73ef08ffc5b55387ed17878fd4ef9dd
Offirmo <offirmo.net@gmail.com>
2015-06-24 08:29:19 +0000

    better appcache + better looking app !

--- client/apps/appcache/index.js
+++ client/apps/appcache/index.js
@@ -2,22 +2,40 @@ window.main = function() {
 	'use strict';
 
 	console.log('Starting index main js...');
+	var server_title = document.title;
 
 	requirejs([
 		'lodash',
 		'carnet',
 		'appcache-nanny',
 		'text!apps/appcache/content.html',
+		'css!apps/appcache/style.css',
 		'angular'
 	],
 	function(_, Carnet, AppCacheNanny, tpl) {
 		console.log('main require done.');
 
+		AppCacheNanny.on('downloading', function() {
+			console.log('downloading', arguments);
+		});
+		AppCacheNanny.on('progress', function() {
+			console.log('progress', arguments);
+		});
+		AppCacheNanny.on('init:downloading', function() {
+			console.log('init:downloading', arguments);
+		});
+		AppCacheNanny.on('init:progress', function() {
+			console.log('init:progress', arguments);
+		});
+		AppCacheNanny.on('init:cached', function() {
+			console.log('init:cached', arguments);
+		});
+
 		// build this app logger
 		var logger = Carnet.make_new({enhanced: true});
 
 		// ui
-		global_ng_module.directive('content', function client() {
+		global_ng_module.directive('contentDirective', function client() {
 			return {
 				template: tpl,
 				replace: true
@@ -26,7 +44,7 @@ window.main = function() {
 
 		global_ng_module.controller('LandingCtrl', function($scope, $document) {
 			logger.info('LandingCtrl…');
-
+			$scope.title = server_title;
 			$scope.update_available = false;
 
 			AppCacheNanny.on('updateready', function handleUpdateready() {
@@ -35,6 +53,7 @@ window.main = function() {
 					$scope.update_available = true;
 				})
 			});
+
 			// check for an update immediately
 			AppCacheNanny.update();
 			// and program periodic update checks
