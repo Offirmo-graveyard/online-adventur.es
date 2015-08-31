9b357bec2eaceebad4ec3d9eccb65d80f40356cf
Offirmo <offirmo.net@gmail.com>
2015-08-31 22:13:54 +0000

    css

--- client/apps/boringrpg/ng/directives/footer/footer.js
+++ client/apps/boringrpg/ng/directives/footer/footer.js
@@ -2,7 +2,7 @@ define([
 	'offirmo-app-bootstrap',
 	'lodash',
 	'text!client/apps/boringrpg/ng/directives/footer/footer.html',
-	//'css!client/apps/boringrpg/ng/directives/footer/footer.css'
+	'css!client/apps/boringrpg/ng/directives/footer/footer.css'
 ],
 function(offirmo_app, _, tpl) {
 	'use strict';
@@ -15,8 +15,36 @@ function(offirmo_app, _, tpl) {
 			},
 			template: tpl,
 			controller: ['$scope', function link($scope) {
+
+				$scope.buttons = [
+					{
+						id: 'explore',
+						icon: 'icomoon-treasure-map'
+					},
+					{
+						id: 'gear',
+						icon: 'icomoon-battle-gear'
+					},
+					{
+						id: 'skills',
+						icon: 'icomoon-death-note'
+					},
+					{
+						id: 'social',
+						icon: 'icomoon-eagle-emblem'
+					},
+					{
+						id: 'feats',
+						icon: 'icomoon-laurel-crown'
+					},
+					{
+						id: 'chat',
+						icon: 'icomoon-conversation'
+					},
+				];
+
 				$scope.flexibleLayoutOptions = {
-					ratios: [1, 1, 1, 1, 1, 1],
+					ratios: _.map($scope.buttons, function(){return 1;}), // everyone equal (for now)
 					direction: 0 // FlexibleLayout.DIRECTION_X
 				};
 
