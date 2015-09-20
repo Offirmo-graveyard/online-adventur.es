a495b96ed38d91c39da26f20b9a49465b39ff022
Offirmo <offirmo.net@gmail.com>
2015-09-20 21:58:13 +0000

    cleanup

--- client/apps/boringrpg/lib/update-watcher.js
+++ client/apps/boringrpg/lib/update-watcher.js
@@ -11,20 +11,20 @@ function(_, Baobab, AppCacheNanny, state_tree) {
 
 	var pending_update_cursor = state_tree.select('update_pending');
 
+
+	function switch_to_updater_if_appropriate() {
+		if (window.offirmo_loader.stage < 2) {
+			window.offirmo_loader.change_stage(3);
+		}
+	}
+
 	function signal_pending_update() {
 		if (! window.offirmo_loader.update_pending) {
 			console.info('update watcher : pending update detected !');
 			window.offirmo_loader.update_pending = true;
 			pending_update_cursor.set(true);
 		}
-	}
-	var auto_update_in_progress = false;
-	function switch_to_updater_if_appropriate() {
-		return;
-		if (window.offirmo_loader.stage < 2) {
-			window.offirmo_loader.change_stage(3);
-			auto_update_in_progress = true;
-		}
+		switch_to_updater_if_appropriate();
 	}
 
 	// https://github.com/gr2m/appcache-nanny
@@ -45,12 +45,10 @@ function(_, Baobab, AppCacheNanny, state_tree) {
 	appCacheNanny.on('downloading', function() {
 		console.log('AppCacheNanny downloading', arguments);
 		signal_pending_update();
-		switch_to_updater_if_appropriate();
 	});
 	appCacheNanny.on('progress', function() {
 		console.log('AppCacheNanny progress', arguments);
 		signal_pending_update();
-		switch_to_updater_if_appropriate();
 	});
 	appCacheNanny.on('cached', function() {
 		console.log('AppCacheNanny cached', arguments);
@@ -59,7 +57,7 @@ function(_, Baobab, AppCacheNanny, state_tree) {
 		// update pending, and all files are downloaded
 		console.log('AppCacheNanny updateready', arguments);
 		signal_pending_update();
-		if (auto_update_in_progress)
+		if (window.offirmo_loader.stage === 3)
 			window.location.reload(true);
 	});
 	AppCacheNanny.on('init:downloading', function() {
@@ -80,30 +78,24 @@ function(_, Baobab, AppCacheNanny, state_tree) {
 		console.log('AppCacheNanny stop');
 	});
 
-	// appcachenany handlers have now replaced our early handlers
+	// appcache nanny handlers have now (hopefully, this is not reliable…) replaced our early handlers.
+	// Transmit possible previous state.
 	pending_update_cursor.set(window.offirmo_loader.update_pending);
-
-	// check for an update immediately
-	//console.log('AppCacheNanny test');
-	//AppCacheNanny.update();
-	//console.log('AN test', AppCacheNanny.hasUpdate());
-	/*if(AppCacheNanny.hasUpdate()) {
-	 return window.location.reload(true);
-	 }*/
-
 	// and program periodic update checks
 	AppCacheNanny.start({
 		loaderPath: 'client/apps/boringrpg/appcache-loader.html',
-		checkInterval: 24 * 60 * 60 * 1000, // ms
+		checkInterval:        24 * 60 * 60 * 1000, // ms
 		offlineCheckInterval: 24 * 60 * 60 * 1000, // ms
 
 	});
-	console.log('AN options', AppCacheNanny.get());
+	//console.log('AN options', AppCacheNanny.get());
 
+	/*
 	console.log('application cache state UNCACHED',    window.applicationCache.UNCACHED);
 	console.log('application cache state IDLE',        window.applicationCache.IDLE);
 	console.log('application cache state CHECKING',    window.applicationCache.CHECKING);
 	console.log('application cache state DOWNLOADING', window.applicationCache.DOWNLOADING);
 	console.log('application cache state UPDATEREADY', window.applicationCache.UPDATEREADY);
 	console.log('application cache state OBSOLETE',    window.applicationCache.OBSOLETE);
+	*/
 });
