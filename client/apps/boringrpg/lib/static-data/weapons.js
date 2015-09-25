488b6c2b5b7bbe0c630567f4a54cdc481ad27be4
Offirmo <offirmo.net@gmail.com>
2015-09-25 20:17:44 +0200

    +

---
+++ client/apps/boringrpg/lib/static-data/weapons.js
@@ -0,0 +1,256 @@
+'use strict';
+
+define({
+	////////////
+	types: {
+		axe: {
+			affinities: {
+
+			}
+		},
+		bow: {
+			affinities: {
+
+			}
+		},
+		claws: {
+			affinities: {
+
+			}
+		},
+		dagger: {
+			affinities: {
+
+			}
+		},
+		grimoire: {
+			affinities: {
+
+			}
+		},
+		harp: {
+			affinities: {
+
+			}
+		},
+		knife: {
+			affinities: {
+
+			}
+		},
+		longbow: {
+			affinities: {
+
+			}
+		},
+		longsword: {
+			affinities: {
+
+			}
+		},
+		luth: {
+			affinities: {
+
+			}
+		},
+		mace: {
+			affinities: {
+
+			}
+		},
+		scythe: {
+			affinities: {
+
+			}
+		},
+		spear: {
+			affinities: {
+
+			}
+		},
+		staff: {
+			affinities: {
+
+			}
+		},
+		sword: {
+			affinities: {
+
+			}
+		},
+		wand: {
+			affinities: {
+
+			}
+		},
+	},
+	////////////
+	primary_qualifiers: {
+		admirable: {
+			affinities: {}
+		},
+		arcanic: {
+			affinities: {}
+		},
+		bestial: {
+			affinities: {}
+		},
+		bone: {
+			affinities: {}
+		},
+		brass: {
+			affinities: {}
+		},
+		cardboard: {
+			affinities: {}
+		},
+		complex: {
+			affinities: {}
+		},
+		composite: {
+			affinities: {}
+		},
+		consecrated: {
+			affinities: {}
+		},
+		crafted: {
+			affinities: {}
+		},
+		cruel: {
+			affinities: {}
+		},
+		cunning: {
+			affinities: {}
+		},
+		cursed: {
+			affinities: {}
+		},
+		emerald: {
+			affinities: {}
+		},
+		engraved: {
+			affinities: {}
+		},
+		forbidden: {
+			affinities: {}
+		},
+		forgotten: {
+			affinities: {}
+		},
+		ghost: {
+			affinities: {}
+		},
+		golden: {
+			affinities: {}
+		},
+		heavy: {
+			affinities: {}
+		},
+		heroic: {
+			affinities: {}
+		},
+		holy: {
+			affinities: {}
+		},
+		inflexible: {
+			affinities: {}
+		},
+		invincible: {
+			affinities: {}
+		},
+		iron: {
+			affinities: {}
+		},
+		jade: {
+			affinities: {}
+		},
+		light: {
+			affinities: {}
+		},
+		living: {
+			affinities: {}
+		},
+		lost: {
+			affinities: {}
+		},
+		mechanical: {
+			affinities: {}
+		},
+		mysterious: {
+			affinities: {}
+		},
+		old: {
+			affinities: {}
+		},
+		onyx: {
+			affinities: {}
+		},
+		overrated: {
+			affinities: {}
+		},
+		powerful: {
+			affinities: {}
+		},
+		practical: {
+			affinities: {}
+		},
+		proven: {
+			affinities: {}
+		},
+		raging: {
+			affinities: {}
+		},
+		robust: {
+			affinities: {}
+		},
+		sapphire: {
+			affinities: {}
+		},
+		savage: {
+			affinities: {}
+		},
+		silver: {
+			affinities: {}
+		},
+		simple: {
+			affinities: {}
+		},
+		sinister: {
+			affinities: {}
+		},
+		skeleton: {
+			affinities: {}
+		},
+		solid: {
+			affinities: {}
+		},
+		steel: {
+			affinities: {}
+		},
+		strange: {
+			affinities: {}
+		},
+		subtile: {
+			affinities: {}
+		},
+		swift: {
+			affinities: {}
+		},
+		unwavering: {
+			affinities: {}
+		},
+		used: {
+			affinities: {}
+		},
+		whirling: {
+			affinities: {}
+		},
+		wooden: {
+			affinities: {}
+		},
+	},
+	////////////
+	secondary_qualifiers: {
+
+	}
+});
