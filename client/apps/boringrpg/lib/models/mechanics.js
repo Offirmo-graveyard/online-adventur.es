681a5477046c6cc77746e7f458d4c61d61c40d2b
Offirmo <offirmo.net@gmail.com>
2015-12-27 08:54:36 +0000

    draft

---
+++ client/apps/boringrpg/lib/models/mechanics.js
@@ -0,0 +1,17 @@
+/** Game numerical computations
+ */
+define([
+	'lodash'
+], function(_, jsen, schema) {
+	'use strict';
+
+	TODO !!!!
+		
+	item score
+
+	item gold value
+
+	click delay (good and bad)
+
+
+});
