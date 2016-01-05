99b9a9e910ee8593dbb6f7ea12572bdd9aef0d50
Offirmo <offirmo.net@gmail.com>
2016-01-05 23:59:05 +0000

    fix inventory bugs : styling and scrolling

--- client/apps/boringrpg/ng/directives/app/content/panels/inventory/inventory.js
+++ client/apps/boringrpg/ng/directives/app/content/panels/inventory/inventory.js
@@ -36,21 +36,15 @@ function(offirmo_app, _, view_static_data, state_tree, model, Weapon, tpl) {
 
 					var inventory_cursor = state_tree.select('model', 'saga', 'inventory');
 
-					/*var maxSurfs = 30;
-					$scope.items = [];
-					for(var i=0; i < maxSurfs; i++) {
-						var weapon = Weapon.create();
-						$scope.items.push({
-							content: 'view #' + (i + 1),
-							weapon: weapon
-						});
-					}
-					*/
-
 					function update_inventory() {
 						$scope.$evalAsync(function () {
 							$scope.inventory = inventory_cursor.get() || [];
-							console.log('* inventory updated :', $scope.inventory);
+							//console.log('* inventory updated :', $scope.inventory);
+							if ($scope.layout) // reduce clip_size if small inventory, to avoid a famous scrollview bug
+								$scope.clip_size = Math.min(
+									$scope.layout.scrollview_size[1],
+									$scope.inventory.length * 24
+								);
 							$scope.selected_index = 0;
 							$scope.selected_item = $scope.inventory.length ? $scope.inventory[$scope.selected_index] : null;
 						});
@@ -68,17 +62,39 @@ function(offirmo_app, _, view_static_data, state_tree, model, Weapon, tpl) {
 				link: function postLink($scope) {
 					var layout_cursor = state_tree.select('view', 'layout', 'panels', 'inventory');
 
+					var inventory_element_styled = false;
+					function style_famous_inventory_element() {
+						if (inventory_element_styled) return;
+
+						try {
+							var inventory_elements = $('.famous-surface.inventory-panel-inventory-entry');
+							if (!inventory_elements.length) {
+								// retry later
+								setTimeout(style_famous_inventory_element, 100);
+								return;
+							}
+
+							var temp1 = $('.famous-surface.inventory-panel-inventory-entry').first();
+							var temp2 = temp1.parent().parent().parent();
+							inventory_element_styled = true;
+							$scope.$evalAsync(function () {
+								temp2.addClass('inventory-panel-inventory');
+							});
+						}
+						catch(err) {
+							console.error('err while styling inventory', err);
+							// retry later
+							setTimeout(style_famous_inventory_element, 100);
+						}
+					}
 					function on_layout_update() {
 						$scope.layout = layout_cursor.get();
+						$scope.clip_size = Math.min(
+							$scope.layout.scrollview_size[1],
+							$scope.inventory ? $scope.inventory.length * 24 : 0
+						);
 
-						$scope.$evalAsync(function () {
-							setTimeout(function () {
-								var temp1 = $('.famous-surface.inventory-panel-inventory-entry').first();
-								var temp2 = temp1.parent().parent().parent();
-								temp2.addClass('inventory-panel-inventory');
-								//console.warn('inventory style', temp1, temp2);
-							}, 100);
-						});
+						setTimeout(style_famous_inventory_element, 100);
 					}
 
 					on_layout_update();
